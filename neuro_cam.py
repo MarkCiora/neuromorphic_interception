17fd5ecdf02870be5b84973b2c11957a800b61c2
Mark Ciora <mfc32@pitt.edu>
2024-02-09 15:56:14 -0500

    asdf

--- neuro_cam.py
+++ neuro_cam.py
@@ -6,6 +6,8 @@ class NVS:
 
     #fov in radians
     #sens will be how sensitive to change
+    spatial_filter = np.asarray([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]], dtype = np.float32)
+    threshold = .3
 
     def __init__(self, res, fov, sens, pos, vel, up):
         self.res = res
@@ -18,7 +20,12 @@ class NVS:
 
     def update_frame(self, object:obj):
         pd = object.pos - self.pos
+        pd_dir = pd / np.linalg.norm(pd)
+        view_width = 2 * np.sin(self.fov * 0.5) * np.linalg.norm(pd)
+        pixel_rad = view_width * 0.5 / self.res
+        print(pd, self.vel)
 
+        new_frame = np.zeros((self.res,self.res), dtype=np.float32)
         for i in range(self.res):
             for j in range(self.res):
                 #check if pixel i,j sees it
@@ -26,4 +33,19 @@ class NVS:
                 phi = j*self.fov/(self.res-1) - self.fov/2
                 dir = self.vel + self.right * theta + self.up * phi
                 dir = dir / np.linalg.norm(dir)
-                print(dir)
+                r = np.arccos(np.dot(pd_dir, dir))
+                r = np.linalg.norm(pd) * np.arctan(r)
+                if r <= object.radius - pixel_rad:
+                    new_frame[i,j] = 1.0
+                elif r <= object.radius + pixel_rad:
+                    lol = (r - object.radius - pixel_rad) * 1.5708 / pixel_rad
+                    new_frame[i,j] = (1.0 - np.cos(lol)) * 0.5
+                else:
+                    new_frame[i,j] = 0.0
+        
+        tframe = new_frame
+        sframe = cv2.filter2D(new_frame, -1, NVS.spatial_filter) * 0.5 + 0.5
+        sframe = (sframe > 0.5 + NVS.threshold).astype(np.int8) - \
+                 (sframe < 0.5 - NVS.threshold).astype(np.int8)
+        sframe = sframe * 127
+        return tframe, sframe
--- run_sim.py
+++ run_sim.py
@@ -1,11 +1,18 @@
 from neuro_cam import NVS
 from object import obj
 import numpy as np
+import cv2
 
-pos = np.asarray([-10, 0, 0], dtype=np.float32)
+pos = np.asarray([-50, 0, 0], dtype=np.float32)
 vel = np.asarray([1, 0, 0], dtype=np.float32)
 up = np.asarray([0, 0, 1], dtype=np.float32)
-cam = NVS(4,0.5,1,pos,vel,up)
-target = obj(np.asarray([0, 0, 0], dtype=np.float32),1)
+cam = NVS(200,0.5,1,pos,vel,up)
+target = obj(np.asarray([0, 1, 2], dtype=np.float32),1)
 
-cam.update_frame(target)
+while True:
+    tframe,sframe = cam.update_frame(target)
+    cv2.imshow("ok", sframe)
+    cam.pos[0] += 1
+
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
+        break
